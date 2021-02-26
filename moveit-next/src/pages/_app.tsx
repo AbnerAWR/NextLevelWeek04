c3fbb729e773f9ff50ba95abf338ef317a4937a8
AbnerAWR <AbnerW_@hotmail.com>
2021-02-26 14:47:03 -0300

    _app update

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,16 +1,10 @@
-import { ChallengeSProvider } from '../contexts/ChallengesContext'
-import { CountdownProvider } from '../contexts/CountdownContext'
-
 import '../styles/global.css'
 
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChallengeSProvider>
-        <Component {...pageProps} />
-    </ChallengeSProvider>
-    
+    <Component {...pageProps} />
   )
 }
 
